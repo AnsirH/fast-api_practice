1862283ea65dffcd7fdc67acc4215e50594bc717
AnsirH <ansir0211@gmail.com>
2025-06-16 10:15:12 +0900

    feat: register_user function in main.py

--- api/main.py
+++ api/main.py
@@ -28,3 +28,35 @@ def get_db():
         yield db # 종속된 함수에 세션 주입
     finally:
         db.close() # 요청이 끝나면 자동으로 세션 종료
+
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, requests
+
+# 템플릿 디렉토리 설정
+# pip install jinja2
+
+
+# 라우터( 요청에 응답하는 )
+# 회원가입 post 시 실행되는 라우터
+@app.post("/api/register")
+def register_user(user: RegisterRequest, db:Session=Depends(get_db)):
+    # 같은 사용자가 있는지 조회
+    existing_user = db.query(User) \
+        .filter(User.username == user.username or User.email == user.email) \
+        .first()
+    # 같은 사용자가 있으면 400에러로 응답
+    if existing_user:
+        raise HTTPException(status_code=400, detail="이미 존재하는 사용자입니다.")
+    # 새 유저에 대한 객체( 인스턴스 ) 생성
+    new_user = User(
+        username = user.username,
+        email = user.email,
+        password = user.password
+    )
+    # db commit하는 과정과 동일
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) # DB에서 자동 생성된 id를 유저 인스턴스에 할당
+    return {"success":True, "message": "회원가입 성공", "user_id": new_user.id}
